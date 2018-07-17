6ca69a0d0e9ba1403d8cac6eee06db70e5a3653b
Alex <alexei.derkach@gmail.com>
2018-07-17 10:59:51 +0300

    Filter class

---
+++ DataBaseMod/DataBase/Filter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NCE.DataBase
+{
+    public class Filter
+    {
+        public DateTime FirstDate { get; set; } = DateTime.Today;
+        public DateTime SecondDate { get; set; } = DateTime.Today;
+        public string Fio { get; set; } = "";
+        public string Shift { get; set; } = "";
+        public string Lot { get; set; } = "";
+        public string LotNumb { get; set; } = "";
+        public string HeatNumb { get; set; } = "";
+        public string SerialNumb { get; set; } = "";
+        public int ControlResult { get; set; } = -1;
+    }
+}
